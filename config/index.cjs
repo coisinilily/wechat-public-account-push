5b3b1fd348bbd9852c7ad04d34c5aaa54d80f2ad
coisinilily <85615735+coisinilily@users.noreply.github.com>
2023-12-24 18:56:58 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'PDU25632TaiAuguloF0KJeg1g4i4fyDE26nPUaYI7',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '001',
+      useTemplateId: '0001',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '11-21',
       festivals: [
