8c0c4a242c7bf6e3d875b634775138e0651103f2
coisinilily <85615735+coisinilily@users.noreply.github.com>
2023-12-24 19:11:34 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -6,7 +6,7 @@ const TEMPLATE_CONFIG = [
     desc: `
       **{{date.DATA}}**
       
-      下个休息日：{{holidaytts.DATA}}
+      // 下个休息日：{{holidaytts.DATA}}
       
       ---
       
@@ -24,7 +24,7 @@ const TEMPLATE_CONFIG = [
       
       ---
       
-      今天是我们相识的第{{love_day.DATA}}天
+      今天是我们相爱的第{{love_day.DATA}}天
       
       {{birthday_message.DATA}}
       
