b80333ff41050fd516d69371bef39c5c7acdfdd4
coisinilily <85615735+coisinilily@users.noreply.github.com>
2023-12-24 19:17:14 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -29,11 +29,12 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝宝', year: '1998', date: '10-03',
-        },
-        {
           type: '*生日', name: '小梅', year: '1998', date: '07-21',
         },
+        {
+          type: '*生日', name: '宝宝', year: '1998', date: '10-03',
+        },
+
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         // {
         //   type: '生日', name: '李四', year: '1996', date: '09-31',
@@ -45,7 +46,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-03-10' },
+        { keyword: 'love_day', date: '2023-03-11' },
         // 结婚纪念日
         // { keyword: 'marry_day', date: '2023-03-11' },
       ],
@@ -63,11 +64,12 @@ const USER_CONFIG = {
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
         {
-          type: '*生日', name: '宝宝', year: '1998', date: '10-03',
-        },
-        {
           type: '*生日', name: '小梅', year: '1998', date: '07-21',
         },
+        {
+          type: '*生日', name: '宝宝', year: '1998', date: '10-03',
+        },
+
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         // {
         //   type: '生日', name: '李四', year: '1996', date: '09-31',
@@ -79,7 +81,7 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2023-03-10' },
+        { keyword: 'love_day', date: '2023-03-11' },
         // 结婚纪念日
         // { keyword: 'marry_day', date: '2023-03-11' },
       ],
