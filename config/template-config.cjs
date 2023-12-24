d8a3c80f9f728ca513ec92e8cc64fb268f1eeb7b
coisinilily <85615735+coisinilily@users.noreply.github.com>
2023-12-24 19:15:42 +0800

    Update template-config.cjs

--- config/template-config.cjs
+++ config/template-config.cjs
@@ -5,9 +5,7 @@ const TEMPLATE_CONFIG = [
     title: '亲爱的, 中午好',
     desc: `
       **{{date.DATA}}**
-      
-      // 下个休息日：{{holidaytts.DATA}}
-      
+
       ---
       
       城市：{{city.DATA}}
